540d11904a1ce992bcb226eb22b06c08e461320c
ttf-platform <maximebertinmourot@hotmail.com>
2026-04-21 14:43:08 +0200

    Add Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
